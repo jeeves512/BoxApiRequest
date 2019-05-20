359ec0a14f2ac8de751ee406298b50a2b2e60879
Jeevan <33185974+jeeves512@users.noreply.github.com>
2019-05-20 11:20:55 +0100

    Update getinfofiltered.py

--- getinfofiltered.py
+++ getinfofiltered.py
@@ -1,25 +1,26 @@
 import requests
 
+# get request is used to get the folder ids of all the files that needs to be renamed
 header = {'Authorization': 'Bearer '+'pVnqHSYrzp03RAlSQ9ZkxrwOhL6H4Xzf'}
-query = 'Test Prime'
+query = 'Test Prime'     # name of file that needs to be renamed
 endpoint = 'https://api.box.com/2.0/search?query='+query+'&type=folder&fields=name'
 get_response = requests.get(endpoint, headers=header)
-output = get_response.json()
-folder_id_array = []
-name_check = False
-print(output)
-for x in output:
+output = get_response.json() # gets the output in json
+folder_id_array = []  #array to store folder ids
+
+# output from the get request contains a lot of data.
+# The output is filtered to find the folder ids of folders matching the query name.
+for x in output:  # traverses the output from get request to store folder id
     if x == 'entries':
         for entries in output[x]:
             for entries_info in entries:
                 if entries_info == 'id' and entries['name'] == query:
                     folder_id_array.append(entries[entries_info])
-                    name_check = False
-
+                    
+                    
+# put request to rename the folders
 put_url = 'https://api.box.com/2.0/folders/'
-put_data = '{"name": "Partner Safeguarding Policies and Procedures Test 123"}'
-for x in folder_id_array:
-    print(x)
-for id in folder_id_array:
+put_data = '{"name": "New Name"}'
+for id in folder_id_array:  # iterates through folder ids to change endpoint of the put request
     put_response = requests.put(put_url+id, data=put_data, headers=header)
-    print(put_response.status_code)
+    print(put_response.status_code)  # print status code to make sure request was successfull
